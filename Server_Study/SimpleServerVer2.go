fc8a0ddcb9a1b81e87f27607371dcd263f34d5f5
tangshiyou <tang.rosechild@gmail.com>
2015-04-12 15:39:58 +0800

    the third edition of the server and the a new FileServer in the XXVer2.go file

--- Server_Study/SimpleServerVer2.go
+++ Server_Study/SimpleServerVer2.go
@@ -4,22 +4,33 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+
 	mux := http.NewServeMux()
-	mux.Handle("/myHandle", &myHandler{})
-	//mux.Handle(pattern, handler)
-	err := http.ListenAndServe(":8089", mux)
+	mux.Handle("/", &myHandle{})
+	wd, err := os.Getwd()
+
 	if err != nil {
 		log.Fatal(err)
 	}
-
+	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir(wd))))
+	err = http.ListenAndServe(":8090", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-type myHandler struct{}
+type myHandle struct{}
 
-func (h *myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "URL: "+r.URL.String())
-	io.WriteString(w, "Hello world\n")
+func (*myHandle) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "URL : "+r.URL.String())
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
